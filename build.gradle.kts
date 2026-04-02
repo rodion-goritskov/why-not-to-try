4872359cf8c45372dc6808a43a6f8c8050f29208
Rodion Goritskov <rodion.goritskov@gmail.com>
2026-04-02 16:20:34 +0200

    Use Junit.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:6.0.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
